fe2a87cfc193706881647ef29ababae30064944c
Amowogbaje Gideon <amowogbajegideon@gmail.com>
2024-07-17 21:05:59 +0100

    test passed api written

---
+++ app/Mail/ContactInquiryMail.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class ContactInquiryMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $data;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->from('amowogbajegideon@gmail.com', 'HNG API Task')
+                    ->subject('New Inquiry from Contact Us Form')
+                    ->view('emails.contact')
+                    ->with('data', $this->data);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::prefix('v1')->group(function () {
+    Route::post('/contact', [ContactController::class, 'sendInquiry']);
+    
+});
+
