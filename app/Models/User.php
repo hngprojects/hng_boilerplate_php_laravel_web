e283d89c609b1c8889a16fdc7d922605b74ec4c4
Olaleye Obidiya <dev.olaleye@smsabuja.com>
2024-07-20 08:09:02 +0100

    Bugfix: merge conflict resolution

--- app/Models/User.php
+++ app/Models/User.php
@@ -93,6 +93,7 @@ class User extends Authenticatable  implements JWTSubject
         return $this->hasOne(Profile::class, 'user_id', 'id');
     }
 
+    // Many to Many relationship with Organisation
     public function organisations(): BelongsToMany
     {
         return $this->belongsToMany(Organisation::class, 'organisation_user', 'user_id', 'org_id')->using(OrganisationUser::class);
