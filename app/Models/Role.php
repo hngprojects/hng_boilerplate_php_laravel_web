4d9c0e0208620869047e1f4abb580c9f489216ff
Abdulbaki Suraj <codelikesuraj@gmail.com>
2024-07-21 13:45:08 +0100

    update: migrations & model for roles & permissions

---
+++ app/Models/Role.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+{
+    use HasFactory;
+
+    public function permissions()
+    {
+        return $this->belongsToMany(Permission::class, 'roles_permissions');
+    }
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'users_roles');
+    }
+}
