65165834bd509cc7ff4e12cc7fde5909fd33f106
Gideon Amowogbaje <amowogbajegideon@gmail.com>
2024-07-21 17:17:12 +0100

    queue

--- app/Mail/ContactInquiryMail.php
+++ app/Mail/ContactInquiryMail.php
@@ -5,8 +5,9 @@ namespace App\Mail;
 use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
 
-class ContactInquiryMail extends Mailable
+class ContactInquiryMail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
