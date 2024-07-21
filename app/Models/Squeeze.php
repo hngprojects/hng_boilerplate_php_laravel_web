d7ed0fa1c8e4b64a8866e523cc840c9064a483d5
NgBlaze <okolaaprecious@gmail.com>
2024-07-21 09:18:18 +0100

    Added mailtrap config and updated squeeze model

--- app/Models/Squeeze.php
+++ app/Models/Squeeze.php
@@ -2,13 +2,19 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Squeeze extends Model
 {
-    use HasFactory, HasUuids;
+    use HasFactory;
 
-    protected $fillable = [];
+    protected $fillable = [
+        'email', 'first_name', 'last_name', 'phone', 'location',
+        'job_title', 'company', 'interests', 'referral_source'
+    ];
+
+    protected $casts = [
+        'interests' => 'array',
+    ];
 }
