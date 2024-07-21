d0235a11741dfbe9f3bfb0b1a9abb92255491d40
Muhammad235 <adelekeyahaya05@gmail.com>
2024-07-20 16:56:47 -1200

    Create: Squeeze model and migration

---
+++ database/migrations/2024_07_21_045055_create_squeezes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('squeezes', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('phone')->nullable();
+            $table->string('location')->nullable();
+            $table->string('job_title')->nullable();
+            $table->string('company')->nullable();
+            $table->string('referral_source')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('squeezes');
+    }
+};
