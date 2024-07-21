7ce47496a26f4064325867a5fec510ea97834bd5
NgBlaze <okolaaprecious@gmail.com>
2024-07-21 10:47:12 +0100

    Added missing 'email' column on my branch

--- app/Models/Squeeze.php
+++ app/Models/Squeeze.php
@@ -4,10 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Squeeze extends Model
 {
     use HasFactory;
+
+    // Set the key type to string
+    protected $keyType = 'string';
+
+    // Disable auto-incrementing IDs
+    public $incrementing = false;
 
     protected $fillable = [
         'email', 'first_name', 'last_name', 'phone', 'location',
@@ -17,4 +24,16 @@ class Squeeze extends Model
     protected $casts = [
         'interests' => 'array',
     ];
+
+    // Automatically generate a UUID when creating a new instance
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (empty($model->{$model->getKeyName()})) {
+                $model->{$model->getKeyName()} = (string) Str::uuid();
+            }
+        });
+    }
 }
