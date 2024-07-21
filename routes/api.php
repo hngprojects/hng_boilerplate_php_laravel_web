66bedecb7f7b1d870990c26c348aa538f03c3728
benjaminharuna1 <benjaminharuna099@gmail.com>
2024-07-21 16:02:46 +0100

    updated routes
    Updtated Routes

--- routes/api.php
+++ routes/api.php
@@ -30,12 +30,12 @@ Route::prefix('v1')->group(function () {
     Route::get('/products/categories', [CategoryController::class, 'index']);
       
     Route::middleware('throttle:10,1')->get('/help-center/topics/search', [ArticleController::class, 'search']);
-
+    Route::put('/notification-settings', [NotificationSettingController::class, 'update']);
 });
 
 
 
 // notifications
 Route::middleware('auth:sanctum')->group(function () {
-    Route::put('/notification-settings', [NotificationSettingController::class, 'update']);
+    
 });
