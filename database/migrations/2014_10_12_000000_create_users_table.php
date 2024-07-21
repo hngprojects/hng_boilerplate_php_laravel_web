8c81eb778bfebff65311e5ca40d3334712111bc7
Muhammad235 <adelekeyahaya05@gmail.com>
2024-07-20 16:41:39 -1200

    Update: users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->boolean('is_active')->default(1);
+            $table->boolean('signup_type')->default(1);
             $table->rememberToken();
             $table->timestamps();
         });
