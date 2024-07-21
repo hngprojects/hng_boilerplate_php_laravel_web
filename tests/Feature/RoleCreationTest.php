538c11ad2c270736472ced4d51733fb8ff850d5a
Olaleye Obidiya <dev.olaleye@smsabuja.com>
2024-07-21 18:54:58 +0100

    Bugfix: debugs role creation integration test

--- tests/Feature/RoleCreationTest.php
+++ tests/Feature/RoleCreationTest.php
@@ -19,7 +19,7 @@ class RoleCreationTest extends TestCase
     protected function setUp(): void
     {
         parent::setUp();
-        
+
         $this->test_user = User::create([
             'name' => 'Test User',
             'email' => 'testuser@example.com',
@@ -28,7 +28,6 @@ class RoleCreationTest extends TestCase
 
         $this->test_org = Organisation::create([
             "name" => 'Test organisation',
-            "user_id" => $this->test_user->id,
             "email" => "test email",
             "description" => "test description",
             "industry" => "test industry",
