5e7b1e8859aaf55b90ac1992ed16e73bfe33e9e7
bhimbho <advancoplanet@gmail.com>
2024-07-21 17:04:16 +0100

    correct squeeze table name

--- database/migrations/2024_07_21_155852_add_email_column_to_squeezes.php
+++ database/migrations/2024_07_21_155852_add_email_column_to_squeezes.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('squeeze', function (Blueprint $table) {
+        Schema::table('squeezes', function (Blueprint $table) {
             $table->string('email')->unique();
         });
     }
@@ -21,7 +21,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('squeeze', function (Blueprint $table) {
+        Schema::table('squeezes', function (Blueprint $table) {
             $table->removeColumn('email');
         });
     }
