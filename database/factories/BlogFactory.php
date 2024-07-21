a218907b408041fe436b5712b510fc3b4afcbfc8
Ezeanyim henry <ezeanyimhenry@gmail.com>
2024-07-21 12:21:07 +0100

    feat: added blog factory and extra column migration

---
+++ database/factories/BlogFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Blog;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Blog>
+ */
+class BlogFactory extends Factory
+{
+    protected $model = Blog::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->sentence,
+            'content' => $this->faker->paragraphs(3, true), 
+            'imageUrl' => $this->faker->imageUrl(), 
+            'tags' => implode(', ', $this->faker->words(5)), 
+            'author' => $this->faker->name,
+        ];
+    }
+}
