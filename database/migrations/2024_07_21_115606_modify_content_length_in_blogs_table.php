98415fd562361d7d4b720c6ef279eaf8d50fd8dd
Ezeanyim henry <ezeanyimhenry@gmail.com>
2024-07-21 12:57:02 +0100

    fix: adjust migration

--- database/migrations/2024_07_21_115606_modify_content_length_in_blogs_table.php
+++ database/migrations/2024_07_21_115606_modify_content_length_in_blogs_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('blogs', function (Blueprint $table) {
-            $table->string('title');
+            $table->text('content')->change();
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('blogs', function (Blueprint $table) {
-            $table->dropColumn('title');
+            $table->string('content')->change();
         });
     }
 };
