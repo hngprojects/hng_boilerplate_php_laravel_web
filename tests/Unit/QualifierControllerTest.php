09c35e85828b5818fa0931cf9133f047f99e7d4a
Charles Mburu Wanjiku <95209529+Charlesmbuu@users.noreply.github.com>
2024-07-22 13:58:41 +0300

    Create QualifierControllerTest.php
    Written tests by Creating a new test file
    
    Signed-off-by: Charles Mburu Wanjiku <95209529+Charlesmbuu@users.noreply.github.com>

---
+++ tests/Unit/QualifierControllerTest.php
@@ -0,0 +1,12 @@
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use App\Http\Controllers\QualifierController;
+
+class QualifierControllerTest extends TestCase
+{
+    public function test_index_returns_qualifiers()
+    {
+        // To implement the test logic to verify the qualifiers are returned
+    }
+}
