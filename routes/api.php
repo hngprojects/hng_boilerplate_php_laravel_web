536d55111e149f0bf38010db960f7111fb85f758
NgBlaze <okolaaprecious@gmail.com>
2024-07-23 10:13:10 +0100

    Updated routes file to contain organization by members route

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,8 @@ use App\Http\Controllers\Api\V1\Testimonial\TestimonialController;
 use App\Http\Controllers\Api\V1\Organisation\OrganisationRemoveUserController;
 use App\Http\Controllers\Api\V1\Auth\LoginController;
 
+use App\Http\Controllers\Api\V1\OrganizationMemberController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -54,19 +56,17 @@ Route::prefix('v1')->group(function () {
         Route::apiResource('/features', FeatureController::class);
         Route::apiResource('/plans', SubscriptionController::class);
         Route::post('/organisations', [OrganisationController::class, 'store']);
+        Route::get('/organization/{organizationId}/members', [OrganizationMemberController::class, 'index']);
     });
-    
+
     Route::middleware('auth.jwt')->group(function () {
         Route::delete('/organizations/{org_id}/users/{user_id}', [OrganisationRemoveUserController::class, 'removeUser']);
     });
     Route::middleware(['auth:api', 'admin'])->get('/customers', [CustomerController::class, 'index']);
 
 
-    
+
     Route::middleware('auth:api')->group(function () {
         Route::post('/testimonials', [TestimonialController::class, 'store']);
     });
-
 });
-
-
