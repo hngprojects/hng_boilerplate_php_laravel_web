d5950b95d64f6a6e8d8e9e40558b6a2606b8e669
yungstarry <111007711+yungstarry@users.noreply.github.com>
2024-07-24 19:04:10 +0100

    feat: user patch

--- routes/api.php
+++ routes/api.php
@@ -32,7 +32,7 @@ use App\Http\Controllers\Api\V1\BlogSearchController;
 |
 | Here is where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
+| be assigned to the "api" midddleware group. Make something great!
 |
 */
 
