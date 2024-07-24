731e7fd6413f87ba53855576670edee86dee8c27
yungstarry <111007711+yungstarry@users.noreply.github.com>
2024-07-24 20:51:33 +0100

    route fix

--- database/migrations/2024_07_18_200734_create_organisations_table.php
+++ database/migrations/2024_07_18_200734_create_organisations_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
             $table->string('country');
             $table->string('address');
             $table->string('state');
-            $table->softDeletes();
+          
             $table->timestamps();
         });
     }
