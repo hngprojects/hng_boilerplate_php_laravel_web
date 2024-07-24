aa89b266e6026f5102f53417ab354055b3735469
sinach <ukaegbuosinachi15@gmail.com>
2024-07-24 23:51:30 +0100

    fix: disable-role & user-deactivation routes modified

--- routes/api.php
+++ routes/api.php
@@ -95,11 +95,13 @@ Route::prefix('v1')->group(function () {
         // Jobs
         Route::get('/jobs', [JobController::class, 'index']);
         Route::get('/user/export/{format}', [ExportUserController::class, 'export']);
+
+        // Accounts
+        Route::patch('/accounts/disable', [AccountController::class, 'deactivate']);
+
+        // Roles
+        Route::put('/organisations/{org_id/roles/{role_id}/disable', [RoleController::class, 'disableRole']);
     });
 
     Route::middleware(['auth:api', 'admin'])->get('/customers', [CustomerController::class, 'index']);
-
-    Route::middleware('auth:api')->patch('/accounts/deactivate', [AccountController::class, 'deactivate']);
-
-    Route::middleware('auth:api')->put('/organisations/{org_id/roles/{role_id}/disable', [RoleController::class, 'disableRole']);
 });
