2a882f520b584be88adb891ce9d9434cf4247354
Muhammad Yahaya <101328329+Muhammad235@users.noreply.github.com>
2024-07-23 19:53:36 -1200

    Update CreateProductRequest.php

--- app/Http/Requests/CreateProductRequest.php
+++ app/Http/Requests/CreateProductRequest.php
@@ -22,7 +22,7 @@ class CreateProductRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => ['required', 'string', 'max:255'],
+            'name' => ['required', 'string', 'max:255', 'unique:products,name'],
             'description' => ['required', 'string'],
            'price' => ['required', 'numeric'],
            'tags' => ['required', 'string'],
