0fc0c88693b7e5a0a3eb5c4c80224f3e2f49cca4
iotstudent <nwanoziep@gmail.com>
2024-07-24 20:19:47 +0100

    route change

--- routes/api.php
+++ routes/api.php
@@ -66,7 +66,7 @@ Route::prefix('v1')->group(function () {
     Route::middleware('auth:api')->group(function () {
         Route::post('/testimonials', [TestimonialController::class, 'store']);
         Route::get('/testimonials/{testimonial_id}', [TestimonialController::class, 'show']);
-        Route::delete('/testimonials/{testimonial_id}', [TestimonialController::class, 'destroy']);
+        Route::delete('/testimonials/{testimonial}', [TestimonialController::class, 'destroy']);
     });
 
 });
