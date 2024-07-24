a5975904770ac26521fd4f912d50a1f72ea81123
bhimbho <advancoplanet@gmail.com>
2024-07-24 03:49:28 +0100

    blog creation endpoint

---
+++ database/factories/BlogImageFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Blog;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\BlogImage>
+ */
+class BlogImageFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'image_url' => $this->faker->imageUrl(),
+            'blog_id' => Blog::factory(),
+        ];
+    }
+}
---
+++ database/factories/BlogTagFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Blog;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\BlogTag>
+ */
+class BlogTagFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->word(),
+            'blog_id' => Blog::factory(),
+        ];
+    }
+}
