82dd78413995f94f9d124d7a5f90ba39d669a053
yungstarry <111007711+yungstarry@users.noreply.github.com>
2024-07-24 13:30:56 +0100

    feat: user patch

--- routes/api.php
+++ routes/api.php
@@ -34,7 +34,7 @@ use App\Http\Controllers\Api\V1\User\ExportUserController;
 |
 | Here is where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
+| be assignedd to the "api" middleware group. Make something great!
 |
 */
 
