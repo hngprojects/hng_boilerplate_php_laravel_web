33915074d24f6ffb5c73744762eef9e7f1e1b948
bhimbho <advancoplanet@gmail.com>
2024-07-25 00:35:05 +0100

    fix: import route class

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\V1\Auth\ResetUserPasswordController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\ContactController;
