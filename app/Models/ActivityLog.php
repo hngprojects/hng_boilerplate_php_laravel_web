2612e26d016a3014be211f7a77eb6b6f966d23f2
adetola-dotbat <adewolenoah4@gmail.com>
2024-07-25 11:20:27 +0100

    feat: implement organization activity logs

--- app/Models/ActivityLog.php
+++ app/Models/ActivityLog.php
@@ -11,6 +11,7 @@ class ActivityLog extends Model
     use HasFactory, HasUuids;
     protected $guarded = ['id'];
 
+
     public function user()
     {
         return $this->belongsTo(User::class);
