8990e4111d1ee9c2b6fdeef9826c8380f6ffd416
Ibrahim Adedayo <ibrahimadedayo@rocketmail.com>
2024-07-25 22:08:55 +0100

    chore: updated doc file to include other endpoints

--- tests/Unit/ProductCreationTest.php
+++ tests/Unit/ProductCreationTest.php
@@ -18,8 +18,8 @@ class ProductCreationTest extends TestCase
         $user = [
             'name' => 'Test User',
             'email' => 'testuser@example.com',
-            'password' => 'password',
-            'password_confirmation' => 'password',
+            'password' => 'Ed8M7s*)?e:hTb^#&;C!<y',
+            'password_confirmation' => 'Ed8M7s*)?e:hTb^#&;C!<y',
         ];
 
         $response = $this->postJson('/api/v1/auth/register', $user);
