12817df497ca532dec809908d5930de098682253
Taiwola <seunolanitori@gmail.com>
2024-07-25 09:18:47 +0100

    chore:resolving conflict

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\Api\V1\Auth\ResetUserPasswordController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\ContactController;
@@ -18,21 +17,12 @@ use App\Http\Controllers\Api\V1\Admin\Plan\FeatureController;
 use App\Http\Controllers\Api\V1\Admin\Plan\SubscriptionController;
 use App\Http\Controllers\Api\V1\Testimonial\TestimonialController;
 
+use App\Http\Controllers\Api\V1\Organisation\OrganisationRemoveUserController;
 use App\Http\Controllers\Api\V1\Auth\LoginController;
+use App\Http\Middleware\LoginAttempts;
 
 use App\Http\Controllers\Api\V1\JobController;
-use App\Http\Controllers\Api\V1\BlogSearchController;
-
 use App\Http\Controllers\Api\V1\User\ExportUserController;
-
-
-use App\Http\Controllers\Api\V1\User\AccountController;
-
-use App\Http\Controllers\Api\V1\Organisation\OrganizationMemberController;
-
-use App\Http\Controllers\InvitationAcceptanceController;
-
-
 
 /*
 |--------------------------------------------------------------------------
@@ -52,13 +42,11 @@ Route::prefix('v1')->group(function () {
     Route::post('/auth/register', [AuthController::class, 'store']);
     Route::post('/auth/login', [LoginController::class, 'login']);
     Route::post('/auth/logout', [LoginController::class, 'logout'])->middleware('auth:api');
-    Route::post('/auth/request-password-request/{token}', ResetUserPasswordController::class);
     Route::post('/roles', [RoleController::class, 'store']);
 
     Route::apiResource('/users', UserController::class);
 
     Route::get('/products/categories', [CategoryController::class, 'index']);
-    Route::get('/products/search', [ProductController::class, 'search']);
 
     Route::middleware('throttle:10,1')->get('/topics/search', [ArticleController::class, 'search']);
 
@@ -76,13 +64,6 @@ Route::prefix('v1')->group(function () {
     Route::get('/blogs/latest', [BlogController::class, 'latest']);
 
     Route::post('/squeeze', [SqueezeController::class, 'store']);
-
-
-    Route::post('/invitations/generate', [InvitationAcceptanceController::class, 'generateInvitation']);
-    Route::get('/invite/accept', [InvitationAcceptanceController::class, 'acceptInvitation']);
-    Route::post('/invite', [InvitationAcceptanceController::class, 'acceptInvitationPost']);
-
-
     Route::middleware('auth:api')->group(function () {
 
         // Subscriptions, Plans and Features
@@ -91,26 +72,15 @@ Route::prefix('v1')->group(function () {
         // Organisations
         Route::post('/organisations', [OrganisationController::class, 'store']);
         Route::get('/organisations', [OrganisationController::class, 'index']);
-        Route::delete('/organisations/{org_id}', [OrganisationController::class, 'destroy']);
         Route::delete('/organisations/{org_id}/users/{user_id}', [OrganisationRemoveUserController::class, 'removeUser']);
-        Route::get('/organisations/{organisation}/members', [OrganizationMemberController::class, 'index']);
-        Route::post('/blogs', [BlogController::class, 'store']);
 
         // Testimonials
         Route::post('/testimonials', [TestimonialController::class, 'store']);
         Route::get('/testimonials/{testimonial_id}', [TestimonialController::class, 'show']);
-        Route::delete('/testimonials/{testimonial}', [TestimonialController::class, 'destroy']);
 
         // Jobs
         Route::get('/jobs', [JobController::class, 'index']);
-
         Route::get('/user/export/{format}', [ExportUserController::class, 'export']);
-
-        // Accounts
-        Route::patch('/accounts/deactivate', [AccountController::class, 'deactivate']);
-
-        // Roles
-        Route::put('/organisations/{org_id/roles/{role_id}/disable', [RoleController::class, 'disableRole']);
     });
 
     Route::middleware(['auth:api', 'admin'])->get('/customers', [CustomerController::class, 'index']);
