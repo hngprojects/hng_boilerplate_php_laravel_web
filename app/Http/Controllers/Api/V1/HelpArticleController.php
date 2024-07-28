308fd95a42fb9d006a710bce752b090c7e08b5c9
NgBlaze <okolaaprecious@gmail.com>
2024-07-28 22:36:41 +0100

    Created Help Article Controller

---
+++ app/Http/Controllers/Api/V1/HelpArticleController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class HelpArticleController extends Controller
+{
+    //
+}
