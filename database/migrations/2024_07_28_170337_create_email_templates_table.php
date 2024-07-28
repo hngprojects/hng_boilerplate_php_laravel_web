66c13ed3a7a0fb9fcffd8b4825475d09f8d1cb01
Olaleye Obidiya <olaleyeobidiya@gmail.com>
2024-07-28 18:13:56 +0100

    Adds notifications tables

---
+++ database/migrations/2024_07_28_170337_create_email_templates_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('email_templates', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('title');
+            $table->text('template');
+            $table->boolean('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('email_templates');
+    }
+};
