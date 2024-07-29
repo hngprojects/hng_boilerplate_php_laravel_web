e22508868bf0b93102ab37b5555f38bde1d6ef1d
NgBlaze <okolaaprecious@gmail.com>
2024-07-29 10:27:25 +0100

    Updated MOdels

--- app/Models/HelpArticle.php
+++ app/Models/HelpArticle.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class HelpArticle extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['user_id', 'title', 'content', 'category'];
+
+    // Define relationship with User model
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
