52fbe0645a22c1261d4b200a11afce84e2b0cdf4
ibrahim <ibrahimadedayo@rocketmail.com>
2024-07-29 12:02:35 +0100

    fix: fix social authentication with google (#252)

--- app/Http/Controllers/Api/V1/Auth/SocialAuthController.php
+++ app/Http/Controllers/Api/V1/Auth/SocialAuthController.php
@@ -24,10 +24,10 @@ class SocialAuthController extends Controller
 
         // Create or update the user
         $user = User::updateOrCreate(
-            ['email' => $googleUser->getEmail],
+            ['email' => $googleUser->email],
             [
                 'password' => Hash::make(Str::password(12)), // Use a random password
-                'social_id' => $googleUser->getId, // Save Google ID for future reference
+                'social_id' => $googleUser->id, // Save Google ID for future reference
                 'is_verified' => true, // Assuming verified on successful social login
                 'signup_type' => 'Google',
                 'is_active' => true,
@@ -38,16 +38,16 @@ class SocialAuthController extends Controller
         // If profile is a separate model, ensure it is created or updated accordingly
         if ($user->profile) {
             $user->profile->update([
-                'first_name' => $googleUser->user['given_name'],
-                'last_name' => $googleUser->user['family_name'],
-                'avatar_url' => $googleUser->user['picture'],
+                'first_name' => $googleUser->name,
+                'last_name' => $googleUser->name,
+                // 'avatar_url' => $googleUser->user['picture'],
             ]);
         } else {
             // Create a profile if it does not exist
             $user->profile()->create([
-                'first_name' => $googleUser->user['given_name'],
-                'last_name' => $googleUser->user['family_name'],
-                'avatar_url' => $googleUser->user['picture'],
+                'first_name' => $googleUser->name,
+                'last_name' => $googleUser->name,
+                // 'avatar_url' => $googleUser->user['picture'],
             ]);
         }
 
@@ -59,13 +59,11 @@ class SocialAuthController extends Controller
             'status' => 'success',
             'message' => 'User successfully authenticated',
             'access_token' => $token,
-            'user' => [
+            'data' => [
                 'id' => $user->id,
                 'email' => $user->email,
-                'name' => $googleUser->user['given_name'] . ' ' . $googleUser->user['family_name'],
-                'given_name' => $googleUser->user['given_name'],
-                'family_name' => $googleUser->user['family_name'],
-                'picture' => $googleUser->user['picture'] // Get the Google profile picture
+                'name' => $googleUser->name,
+                'google' => $googleUser
             ]
         ];
 
