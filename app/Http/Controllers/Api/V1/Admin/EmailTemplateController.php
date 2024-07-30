77d5752757c034f2c491be162ff5dad20466f0e1
wilsonabdiel <wilsonabdiel86@gmail.com>
2024-07-30 08:19:30 +0100

    refactor: moved email template controller to admin

--- app/Http/Controllers/Api/V1/Admin/EmailTemplateController.php
+++ app/Http/Controllers/Api/V1/Admin/EmailTemplateController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api\V1\Admin;
 
+use App\Http\Controllers\Controller;
 use App\Models\EmailTemplate;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
