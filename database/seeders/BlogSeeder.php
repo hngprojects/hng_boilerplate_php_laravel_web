734fd2604012d96c67e6c248057ec7d625ba317c
Farouq Akinkunmi Akinola <akinolaakinkunmifa@gmail.com>
2024-07-30 12:46:18 +0100

    fix: fixed test failed error and added images to blog seeder

--- database/seeders/BlogSeeder.php
+++ database/seeders/BlogSeeder.php
@@ -4,8 +4,10 @@ namespace Database\Seeders;
 
 use App\Models\Blog;
 use App\Models\BlogCategory;
+use App\Models\BlogImage;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Http\UploadedFile;
 
 class BlogSeeder extends Seeder
 {
@@ -22,7 +24,12 @@ class BlogSeeder extends Seeder
                 'description' => $category.' is a blog category. learn more and explore'
             ]);
 
-            Blog::factory()->create(['blog_category_id' => $blog_category->id]);
+            $blog = Blog::factory()->create(['blog_category_id' => $blog_category->id]);
+            
+            $blog_image = BlogImage::create([
+                'image_url' => json_encode([UploadedFile::fake()->image('image1.jpg')->getClientOriginalName()]),
+                'blog_id' => $blog->id
+            ]);
         }
     }
 }
