68aa9ca22244822aa0edd60425b1a2a6c321e3a1
olakadasami <olakadasamuel@gmail.com>
2024-07-30 22:39:44 +0100

    feat: add createNotification method

--- app/Http/Controllers/UserNotificationController.php
+++ app/Http/Controllers/UserNotificationController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\NotificationCreated;
+use App\Http\Requests\CreateNotificationRequest;
 use App\Models\Notification;
+use App\Models\User;
 use App\Models\UserNotification;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Str;
 
 class UserNotificationController extends Controller
 {
@@ -22,10 +27,49 @@ class UserNotificationController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(CreateNotificationRequest $request)
     {
-        //
+        // Query the users to send notifications to
+        $users = User::all();
+
+        $newNotification = DB::transaction(function () use ($request, $users) {
+            // Create Notification
+            $notification = Notification::create([
+                'type' => 'push',
+                'title' => 'notification',
+                'message' => $request->input('message'),
+            ]);
+
+            // Loop through each user
+            foreach ($users as $user) {
+                $user->notifications()->attach($notification->id, [
+                    'id' => (string) Str::uuid(),
+                    'status' => 'unread'
+                ]);
+
+                // Dispatch notification
+                // if ($notification->type === 'push') {
+                //     // create new pusher event for internal notifications
+                //     event(new NotificationCreated($notification));
+                // }
+            }
+            return $notification;
+        });
+
+        // Return 201
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Notification created successfully',
+            'status_code' => 201,
+            'data' => [
+                'id' => $newNotification->id,
+                // 'user_id' => $newNotification->id, // Todo
+                'message' => $newNotification->message,
+                'created_at' => $newNotification->created_at,
+            ]
+        ], 201);
     }
+
 
     /**
      * Store a newly created resource in storage.
@@ -70,7 +114,6 @@ class UserNotificationController extends Controller
                 'status_code' => Response::HTTP_OK,
                 'data' => $notification,
             ], Response::HTTP_OK);
-
         } catch (ModelNotFoundException $exception) {
             return response()->json([
                 'status' => 'error',
@@ -107,7 +150,6 @@ class UserNotificationController extends Controller
                 'status_code' => Response::HTTP_OK,
                 'data' => UserNotification::query()->where('user_id', auth()->id())->get(),
             ], Response::HTTP_OK);
-
         } catch (ModelNotFoundException $exception) {
             return response()->json([
                 'status' => 'error',
