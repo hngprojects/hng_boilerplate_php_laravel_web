e030de855ffd21c78a39623b4d251c199208f8a6
Ibrahim Adedayo <ibrahimadedayo@rocketmail.com>
2024-07-31 09:39:02 +0100

    feta: added facebook login

--- app/Http/Controllers/Api/V1/Auth/SocialAuthController.php
+++ app/Http/Controllers/Api/V1/Auth/SocialAuthController.php
@@ -69,4 +69,56 @@ class SocialAuthController extends Controller
 
         return response()->json($response);
     }
+
+    public function loginUsingFacebook()
+    {
+        return Socialite::driver('facebook')->stateless()->redirect();
+    }
+
+    public function callbackFromFacebook()
+    {
+        try {
+            $facebookUser = Socialite::driver('facebook')->user();
+
+            // $user = User::updateOrCreate(
+            //     ['email' => $facebookUser->email],
+            //     [
+            //         'password' => Hash::make(Str::password(12)), // Use a random password
+            //         'social_id' => $facebookUser->id, // Save Google ID for future reference
+            //         'is_verified' => true, // Assuming verified on successful social login
+            //         'signup_type' => 'Facebook',
+            //         'is_active' => true,
+            //     ]
+            // );
+
+            /* $user = User::updateOrCreate([
+                'social_id' => $facebookUser->getId(),
+            ],[
+                'name' => $facebookUser->getName(),
+                'email' => $facebookUser->getEmail(),
+                'password' => Hash::make($user->getName().'@'.$facebookUser->getId())
+            ]); */
+
+            // Auth::loginUsingId($saveUser->id);
+            // $token = JWTAuth::fromUser($user);
+
+            $response = [
+                'status' => 'success',
+                'message' => 'User successfully authenticated',
+                // 'access_token' => $token,
+                'facebook' => $facebookUser
+                /* 'data' => [
+                    'id' => $user->id,
+                    'email' => $user->email,
+                    'first_name' => $googleUser->user['given_name'],
+                    'last_name' => $googleUser->user['family_name']
+                ] */
+            ];
+    
+            return response()->json($response);
+            
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+    }
 }
