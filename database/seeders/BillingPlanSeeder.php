96d3a05d671bf85b0fdb4f510795fa53bad9c047
Alemoh Rapheal <raphealenike@gmail.com>
2024-07-31 03:06:59 -0700

    fix: update the billing plan seeder to create all  plans

--- database/seeders/BillingPlanSeeder.php
+++ database/seeders/BillingPlanSeeder.php
@@ -18,7 +18,7 @@ class BillingPlanSeeder extends Seeder
             [
                 'id' => Str::uuid()->toString(),
                 'name' => 'Free',
-                'price' => 0.00,
+                'price' => 0,
                 'features' => json_encode(['Stage 1', 'Stage 2', 'Stage 3']),
                 'description' => 'Free plan with basic features.',
                 'created_at' => now(),
@@ -26,10 +26,28 @@ class BillingPlanSeeder extends Seeder
             ],
             [
                 'id' => Str::uuid()->toString(),
+                'name' => 'Basic',
+                'price' => 20,
+                'features' => json_encode(['100 projects', 'up to 50 subscribers', 'Advanced analytics', '24-hour support']),
+                'description' => 'Ideal for growing needs who want more features',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'id' => Str::uuid()->toString(),
+                'name' => 'Advanced',
+                'price' => 50,
+                'features' => json_encode(['200 projects', 'up to 100 subscribers', 'Advanced analytics', '24-hour support', 'Marketing advisor']),
+                'description' => 'Designed for power users and maximum functionalities',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'id' => Str::uuid()->toString(),
                 'name' => 'Premium',
-                'price' => 300.00,
-                'features' => json_encode(['Premium HNG', 'Premium Gen-z']),
-                'description' => 'Premium plan with advanced features.',
+                'price' => 100,
+                'features' => json_encode(['300 projects', 'up to 500 subscribers', 'Advanced analytics', '24-hour support', 'Marketing advisor']),
+                'description' => 'Perfect for users who want more features',
                 'created_at' => now(),
                 'updated_at' => now()
             ]
