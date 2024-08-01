097a84c0ad98252172fc5a000e40842bae2306c9
bamo100 <bankolediamond10@gmail.com>
2024-08-01 09:59:40 +0100

    fix2

--- tests/Feature/ProductControllerTest.php
+++ tests/Feature/ProductControllerTest.php
@@ -1,17 +1,2 @@
-<?php
 
-namespace Tests\Feature;
-
-
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Tests\TestCase;
-use App\Models\Product;
-use App\Models\Category;
-use App\Models\ProductVariant;
-use App\Models\User;
-
-class ProductControllerTest extends TestCase
-{
-    
-}
 
--- tests/Feature/ProductTest.php
+++ tests/Feature/ProductTest.php
@@ -1,12 +1,1 @@
-<?php
-namespace Tests\Unit;
 
-use Tests\TestCase;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use App\Models\User;
-use App\Models\Product;
-
-class ProductTest extends TestCase
-{
-   
-}
