36db6b73bc9540961385d12751ad8d9c7b5e73b2
Olaleye Obidiya <olaleyeobidiya@gmail.com>
2024-08-01 15:47:39 +0100

    feat: makes factory and migration folder changes
    Changed some columns on squeezes tables
    Added factory files for UserSubscription and SubscriptionPlan etc.

---
+++ database/factories/BlogCategoryFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\BlogCategory>
+ */
+class BlogCategoryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'id' => Str::uuid(),
+            'name' => $this->faker->word,
+            'description' => $this->faker->text
+        ];
+    }
+}
