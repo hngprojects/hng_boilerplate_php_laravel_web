d507f30385e05461f5513f61e003ef3b1e5abca5
Farouq Akinkunmi Akinola <akinolaakinkunmifa@gmail.com>
2024-08-01 14:21:17 +0100

    Added real images for seeder

--- database/seeders/BlogSeeder.php
+++ database/seeders/BlogSeeder.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class BlogSeeder extends Seeder
 {
@@ -16,9 +17,20 @@ class BlogSeeder extends Seeder
     public function run(): void
     {
         $categories = ['Business', 'Food', 'Lifestyle', 'World News'];
+        $images = [
+            'https://free-images.com/lg/dd2c/port_au_prince_haiti.jpg',
+            'https://free-images.com/lg/45e3/squirrel_tree_mammal_paw.jpg',
+            'https://free-images.com/lg/556a/netherlands_landscape_sky_clouds.jpg',
+            'https://free-images.com/lg/4275/penguin_funny_blue_water.jpg'
+        ];
 
-        foreach($categories as $category){
-            $blog = Blog::factory()->create(['category' => $category]);
+        foreach($categories as $key=>$category){
+            $imageContents = file_get_contents($images[$key]);
+            $imageName = Str::random(10) . '.jpg';
+            Storage::disk('public')->put('images/' . $imageName, $imageContents);
+            $imagePath = 'storage/images/' . $imageName;
+
+            $blog = Blog::factory()->create(['category' => $category, 'image_url' => $imagePath]);
         }
     }
 }
