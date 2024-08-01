ab576860a0c81b402b0ea83918f0b687fa32f88c
Amowogbaje Gideon <amowogbajegideon@gmail.com>
2024-08-01 01:39:14 +0100

    payment in progress

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -9,4 +9,28 @@ use Illuminate\Database\Eloquent\Model;
 class Payment extends Model
 {
     use HasFactory, HasUuids;
+
+    protected $fillable = [
+        'user_id',
+        'transaction_id',
+        'gateway_id',
+        'amount',
+        'status'
+    ];
+
+    protected $casts = [
+        'id' => 'uuid',
+        'user_id' => 'uuid',
+        'gateway_id' => 'uuid',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function gateway()
+    {
+        return $this->belongsTo(Gateway::class);
+    }
 }
