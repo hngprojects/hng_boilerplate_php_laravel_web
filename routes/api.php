9ceba84cacfb020becc17882857674774ec1b8bb
Ezeanyim henry <ezeanyimhenry@gmail.com>
2024-08-01 11:32:34 +0100

    feat: store email templates

--- routes/api.php
+++ routes/api.php
@@ -122,6 +122,7 @@ Route::prefix('v1')->group(function () {
 
     Route::middleware(['auth:api', 'admin'])->group(function () {
         Route::get('/email-templates', [EmailTemplateController::class, 'index']);
+        Route::post('/email-templates', [EmailTemplateController::class, 'store']);
         Route::patch('/email-templates/{id}', [EmailTemplateController::class, 'update']);
     });
 
