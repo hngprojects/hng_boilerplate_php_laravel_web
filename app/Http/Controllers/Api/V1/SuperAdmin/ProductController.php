ffa776499278d34de3ccf5957c20ede0c18dd4db
NgBlaze <okolaaprecious@gmail.com>
2024-08-06 13:53:03 +0100

    Created super admin products controller

---
+++ app/Http/Controllers/Api/V1/SuperAdmin/ProductController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\SuperAdmin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    //
+}
