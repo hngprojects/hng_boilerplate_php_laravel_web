1f49eb6ecacaa6c7819b6ebcb49724638ed69e99
timiajayi <timiajayi2001@gmail.com>
2024-08-08 17:34:13 +0100

    view all top produts

--- routes/api.php
+++ routes/api.php
@@ -247,6 +247,10 @@ Route::prefix('v1')->group(function () {
         Route::get('/statistics', [AdminDashboardController::class, 'getStatistics']);
         Route::apiResource('faqs', FaqController::class);       
         Route::get('/dashboard/top-products', [AdminDashboardController::class, 'getTopProducts']);        
+        Route::get('/all-products-by-sales', [AdminDashboardController::class, 'getAllProductsSortedBySales']);
+
+
+       
     });
 
     Route::post('/waitlists', [WaitListController::class, 'store']);
