53a6ed9a08d99bac76a05031d18b990ec7204d4d
Ibrahim Adedayo <ibrahimadedayo@rocketmail.com>
2024-08-09 16:17:16 +0100

    fix: google auth

--- app/Http/Controllers/Api/V1/Auth/SocialAuthController.php
+++ app/Http/Controllers/Api/V1/Auth/SocialAuthController.php
@@ -5,13 +5,13 @@ namespace App\Http\Controllers\Api\V1\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Laravel\Socialite\Facades\Socialite;
-use Illuminate\Support\Facades\Auth;
 use Tymon\JWTAuth\Facades\JWTAuth;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Validator;
 use Google_Client;
+use Illuminate\Support\Facades\Http;
 
 class SocialAuthController extends Controller
 {
@@ -130,67 +130,66 @@ class SocialAuthController extends Controller
         // Extract Google user data from the request
         $idToken = $request->id_token;
 
-        $client = new Google_Client(['client_id' => env('GOOGLE_CLIENT_ID')]); // Replace with your Google Client ID
-        $payload = $client->verifyIdToken($idToken);
+        $response = Http::get("https://www.googleapis.com/oauth2/v3/tokeninfo?id_token={$idToken}");
+        if($response->successful()) {
+            $payload = $response->json();
+            
+            if (isset($payload['sub']) && isset($payload['email'])) {
+                $email = $payload['email'];
+                $firstName = $payload['given_name'];
+                $lastName = $payload['family_name'];
+                $avatarUrl = $payload['picture'] ?? null;
 
-        if ($payload) {
-             // Token is valid
-            $email = $payload['email'];
-            $firstName = $payload['given_name'];
-            $lastName = $payload['family_name'];
-            $avatarUrl = $payload['picture'] ?? null;
+                // Create or update user
+                $user = User::updateOrCreate(
+                    ['email' => $email],
+                    [
+                        'password' => Hash::make(Str::random(12)), // Generate a random password for the user
+                        'social_id' => $idToken,
+                        'is_verified' => true,
+                        'signup_type' => 'Google',
+                        'is_active' => true,
+                    ]
+                );
 
-            // Create or update user
-            $user = User::updateOrCreate(
-                ['email' => $email],
-                [
-                    'password' => Hash::make(Str::random(12)), // Generate a random password for the user
-                    'social_id' => $idToken,
-                    'is_verified' => true,
-                    'signup_type' => 'Google',
-                    'is_active' => true,
-                    'role' => 'user',
-                ]
-            );
+                // Update or create user profile
+                if ($user->profile) {
+                    $user->profile->update([
+                        'first_name' => $firstName,
+                        'last_name' => $lastName,
+                        'avatar_url' => $avatarUrl,
+                    ]);
+                } else {
+                    $user->profile()->create([
+                        'first_name' => $firstName,
+                        'last_name' => $lastName,
+                        'avatar_url' => $avatarUrl,
+                    ]);
+                }
 
-            // Update or create user profile
-            if ($user->profile) {
-                $user->profile->update([
-                    'first_name' => $firstName,
-                    'last_name' => $lastName,
-                    'avatar_url' => $avatarUrl,
-                ]);
-            } else {
-                $user->profile()->create([
-                    'first_name' => $firstName,
-                    'last_name' => $lastName,
-                    'avatar_url' => $avatarUrl,
-                ]);
-            }
+                $token = JWTAuth::fromUser($user);
 
-            // Generate JWT token
-            $token = JWTAuth::fromUser($user);
-
-            return response()->json([
-                'status_code' => 200,
-                'message' => 'User Created Successfully',
-                'access_token' => $token,
-                'data' => [
-                    'user' => [
+                return response()->json([
+                    'status_code' => 200,
+                    'message' => 'User Created',
+                    'access_token' => $token,
+                    'data' => [
                         'id' => $user->id,
                         'email' => $user->email,
                         'first_name' => $firstName,
                         'last_name' => $lastName,
-                        'fullname' => $firstName.' '.$lastName,
-                        'role' => $user->role,
                     ]
-                ]
-            ]);
+                ]);
+            } else {
+                return response()->json([
+                    'status_code' => 401,
+                    'message' => 'Invalid Token Payload'
+                ], 401);
+            }
         } else {
-            // Invalid token
             return response()->json([
                 'status_code' => 401,
-                'message' => 'Invalid Token'
+                'message' => 'Invalid Token: ' . $response->body()
             ], 401);
         }
     }
--- tests/Unit/RegistrationTest.php
+++ tests/Unit/RegistrationTest.php
@@ -163,6 +163,31 @@ class RegistrationTest extends TestCase
     }
 
     /** @test */
+    public function google_login_creates_or_updates_user_and_profile_with_post_duplicate()
+    {
+        // Mock Google user response
+        $token = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjQ1MjljNDA5Zjc3YTEwNmZiNjdlZTFhODVkMTY4ZmQyY2ZiN2MwYjciLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhenAiOiI2OTM4ODEwMzIwNTYtNXBmOGNiNnZuYXU4ZDMyc2tlbjE3MnZmN3VxNTk4NWcuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJhdWQiOiI2OTM4ODEwMzIwNTYtNXBmOGNiNnZuYXU4ZDMyc2tlbjE3MnZmN3VxNTk4NWcuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJzdWIiOiIxMTcxODk1ODY5NDkyOTk5NDA1OTMiLCJlbWFpbCI6ImF5b21pa3VudGVtaXRvcGUyNDZAZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImF0X2hhc2giOiJ5cVdyTGt4MkJHZkVVc3dFQ2ZJUFNnIiwibmFtZSI6IlBob2VuaXgiLCJwaWN0dXJlIjoiaHR0cHM6Ly9saDMuZ29vZ2xldXNlcmNvbnRlbnQuY29tL2EvQUNnOG9jSmt4c0RGRVVaWFJNSGE4Q1RrcUVSVWE1TUk2c3RyNXFyc1FUWS1lVmdlamJxVXFUX3g9czk2LWMiLCJnaXZlbl9uYW1lIjoiUGhvZW5peCIsImlhdCI6MTcyMzE1MTE2MiwiZXhwIjoxNzIzMTU0NzYyfQ.uSO2sjuUtEZ19o7BMJk7qt9nLhPe62N7y9aeY8P_2IZE1_IdNKYR0uUrqCP_t6ylbQBYvaE5O8zHNmDqjsmI0wjxsTMnnLaaN2zU-00zoyOa8ySl1UlGNaWZE3XXC8Wa43FQrOgnTTxZUYzZJxsd-qB2KDCHmnBAtTlT_HVHgdasdBpUjCg-y_M5v42tMs-MJD18IpU4LTi2nz3nqrgdbEbPq0otnQ1HnQqVDEAsoHgSeXSu6GUFof_4WTvztc5xDzRUnstGrbinoxeuP-7lPaTLYAB6YbX1bLFUnfKKbKZ3XeVRz1KGpvDDyQjhxNfCQ0iitF0otsVKfrmdmID0pg";
+        // $token = "ya29.a0AcM612zwlgIkLYEu-VxLuxkvwubv75ffSHPVxGLJFx--X-mO0CUz6hmK7CV4ilJt9EHD-JkWlGzgrD0PpDc4w0DSWTPCR4HY4UYuWxr1hHJI_RIectNFBse5xb6HVpzPxS8LKKQPW5MBQWO5-5A8sFolmRPo3L4LBspUaCgYKAbkSARESFQHGX2MicoD2zJRxO-CMjQndJSFNow0171";
+
+        $response = $this->postJson('/api/v1/auth/google', [
+            'id_token' => $token
+        ]);
+
+        dd($response);
+
+        $response->assertStatus(Response::HTTP_OK);
+
+        // Verify user in the database
+        $user = User::where('email', $googleUser['email'])->first();
+        $this->assertNotNull($user);
+        $this->assertEquals($googleUser['id'], $user->social_id);
+        $this->assertEquals('Google', $user->signup_type);
+        $this->assertEquals('John', $user->profile->first_name);
+        $this->assertEquals('Doe', $user->profile->last_name);
+        $this->assertEquals($googleUser['attributes']['avatar_original'], $user->profile->avatar_url);
+    }
+
+    /** @test */
     public function facebook_login_creates_or_updates_user_and_profile()
     {
         // Mock Facebook user response
