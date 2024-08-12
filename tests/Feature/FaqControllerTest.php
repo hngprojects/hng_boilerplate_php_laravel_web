b89ecaa99b5819061e73015accc989a7adb7d7b3
Timiajayi <61969771+timiajayi@users.noreply.github.com>
2024-08-12 13:34:43 +0100

    Update FaqControllerTest.php
    Signed-off-by: Timiajayi <61969771+timiajayi@users.noreply.github.com>

--- tests/Feature/FaqControllerTest.php
+++ tests/Feature/FaqControllerTest.php
@@ -19,7 +19,7 @@ class FaqControllerTest extends TestCase
     {
         parent::setUp();
         $this->superAdmin = User::factory()->create(['role' => 'superadmin']);
-        $this->token = JWTAuth::fromUser($this->superadmin);
+        $this->token = JWTAuth::fromUser($this->superAdmin);
     }
 
     public function test_super_admin_can_create_faq()
