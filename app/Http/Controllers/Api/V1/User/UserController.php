08ff694b2ac819c5ae5220d1eb94746d4cfbc2a6
wilsonabdiel <wilsonabdiel86@gmail.com>
2024-08-15 22:22:15 +0100

    fix: adjusting payload to match nestjs

--- app/Http/Controllers/Api/V1/User/UserController.php
+++ app/Http/Controllers/Api/V1/User/UserController.php
@@ -34,7 +34,7 @@ class UserController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index(Request $request)
+    public function index()
     {
         $users = User::paginate(15);
         
