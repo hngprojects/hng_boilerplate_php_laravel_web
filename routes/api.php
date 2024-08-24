6614a1da78e9290cf6fd471260dc31423b14aaee
Amowogbaje Gideon <amowogbajegideon@gmail.com>
2024-08-24 22:33:38 +0100

    product get

--- routes/api.php
+++ routes/api.php
@@ -109,7 +109,7 @@ Route::prefix('v1')->group(function () {
     Route::get('/products/categories', [CategoryController::class, 'index']);
     Route::get('/products/search', [ProductController::class, 'search']);
     Route::get('/products', [ProductController::class, 'index']);
-    Route::get('/products/{product_id}', [ProductController::class, 'show']);
+    Route::get('/products/{product_id}', [ProductController::class, 'showProduct']);
     Route::delete('/products/{product_id}', [ProductController::class, 'delete']);
 
     Route::get('/billing-plans', [BillingPlanController::class, 'index']);
