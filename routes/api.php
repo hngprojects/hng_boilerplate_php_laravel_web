1f59b67ffb09e80e1690da062dcc89a7af03aeed
Ibrahim Adedayo <ibrahimadedayo@rocketmail.com>
2024-08-24 23:35:44 +0100

    feat: added update of subscription plan

--- routes/api.php
+++ routes/api.php
@@ -112,6 +112,7 @@ Route::prefix('v1')->group(function () {
     Route::get('/products/{product_id}', [ProductController::class, 'show']);
     Route::get('/billing-plans', [BillingPlanController::class, 'index']);
     Route::get('/billing-plans/{id}', [BillingPlanController::class, 'getBillingPlan']);
+    Route::put('/billing-plans/{id}', [BillingPlanController::class, 'update']);
     Route::get('/payments/paystack/{organisation_id}/verify/{id}', [PaymentController::class, 'handlePaystackCallback']);
     Route::get('/payments/flutterwave/{organisation_id}/verify/{id}', [PaymentController::class, 'handleFlutterwaveCallback']);
     Route::post('/languages', [LanguageController::class, 'create']);
